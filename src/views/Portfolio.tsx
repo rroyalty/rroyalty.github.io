8012e8054f2adc2e09eaa429452871e02622e6e8
rroyalty <rroyalty@gmail.com>
2021-05-10 14:02:47 -0400

    remove old stuff

--- src/views/Portfolio.tsx
+++ src/views/Portfolio.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withRouter } from 'react-router';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 const Portfolio: React.FC = (): JSX.Element => {
