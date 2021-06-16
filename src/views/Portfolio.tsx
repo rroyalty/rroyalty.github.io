261f19fd5e9b646b3ea5e0651afe37410423e67b
rroyalty <rroyalty@gmail.com>
2021-06-16 15:34:17 -0400

    cleaning it up

--- src/views/Portfolio.tsx
+++ src/views/Portfolio.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router';
 import ProjectCard from '../components/ProjectCard'
 import projectList from '../static/projects.json'
-import { Grid } from "@material-ui/core"
+import { createStyles, makeStyles, Theme, GridList, Container, Grid } from '@material-ui/core';
 
 // interface IProps {
 //     name: string,
@@ -13,23 +13,62 @@ import { Grid } from "@material-ui/core"
 //     icon: string
 // }
 
-console.log(projectList)
+const useStyles = makeStyles(() =>
+    createStyles({
+        root: {
+            overflow: `hidden`,
+            justifyContent: 'center',
+            alignItems: 'center',
+            maxWidth: `95vw`,
+            border: `2px`,
+            borderStyle: `solid`,
+            borderColor: `white`,
+            padding: `0px`,
+            maxHeight: `100vh`
+        },
+        img: {
+            display: `flex`,
+            backgroundSize: "cover",
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            height: 'auto',
+            minHeight: '100vh',
+            justifyContent: `center`,
+        },
+        gridList: {
+            paddingTop: `20px`,
+            display: `flex`,
+            justifyContent: 'center',
+            alignItems: 'top',
+            overflowX: 'hidden',
+            maxHeight: `100vh`,
+            overflow: `auto`,
+            width: `100%`
+        },
+        gridItem: {
+            width: `100%`
+        },
+    }),
+);
+
 
 const Portfolio: React.FC = (): JSX.Element => {
+    const classes = useStyles();
+
     return (
         <Grid
             container
             direction="row"
             justify="center"
             alignItems="center"
-        >
-            {projectList.map(item => (
-                <Grid item xs={4}>
+        >    <GridList className={classes.gridList} cellHeight={160} >
+                {projectList.map(item => (
                     <ProjectCard key={item.name} props={item} />
-                </Grid>
-            ))}
+                ))}
+            </GridList>
         </Grid>
     )
 }
+
 
 export default withRouter(Portfolio)
