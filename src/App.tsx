fbfa3fcf56525d06edc7221a02c4c3589ed73cb0
rroyalty <rroyalty@gmail.com>
2021-06-16 15:46:59 -0400

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,41 @@ import Navbar from './components/Navbar';
 import Home from './views/Home';
 import Bio from './views/Bio';
 import Portfolio from './views/Portfolio';
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
 import './App.css';
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    display: `flex`,
+    backgroundSize: "cover",
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    width: "100vw",
+    justifyContent: `center`,
+
+  },
+  footer: {
+    backgroundColor: theme.palette.primary.main,
+    display: `flex`,
+  }
+}));
+
+
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Router>
-      <>
-
-        <Container maxWidth='xl' className="App bg" style={{
-          backgroundImage: `url(/images/code-Background.jpg)`,
-          backgroundSize: "cover",
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          height: "100vh",
-
-        }}>
-        <Navbar />
-          <Switch>
-            <Route exact path="/index" component={Home} />
-            <Route exact path="/bio" component={Bio} />
-            <Route exact path="/portfolio" component={Portfolio}/>
-            <Route exact path="/" component={Home} />
-          </Switch>
-
-        </Container>
-
-      </>
+        <>
+          <Navbar />
+          <Container id="bio" className={classes.root} maxWidth='xl' style={{ backgroundImage: `url(/backgrounds/code-Background.jpg)` }} >
+            <Bio />
+          </Container>
+          <Container id="portfolio" className={classes.root} maxWidth='xl' style={{ backgroundImage: `url(/backgrounds/code-Background.jpg)` }} >
+            <Portfolio />
+          </Container>
+        </>
     </Router>
   );
 }
