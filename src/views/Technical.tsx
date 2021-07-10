22c2004d118be0ee83ecd86b2c292c8455f8fc7a
rroyalty <rroyalty@gmail.com>
2021-07-10 08:50:01 -0400

    restructure

--- src/views/Technical.tsx
+++ src/views/Technical.tsx
@@ -30,7 +30,7 @@ const Technical: React.FC = (): JSX.Element => {
     return (
         <>
             <Typography className={classes.typography} align="center" variant="h2">
-                SKILLS
+                FRONT TO BACK
             </Typography>
 
             <Grid
