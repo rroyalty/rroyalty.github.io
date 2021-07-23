3358e5279c87587238494adb517cae09ca4ed7ce
rroyalty <rroyalty@gmail.com>
2021-07-23 11:25:28 -0400

    test

--- src/API/API.ts
+++ src/API/API.ts
@@ -4,7 +4,7 @@ const API = {
     getNewestText: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestText`) },
     getNewestTitle: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestTitle`) }, 
     getNewestDate: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestDate`) },
-    getAllPosts: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/allPosts`) }  
+    getAllPosts: async () => { return await axios.get(`https://rrprofile.herokuapp.com/api/self/allPosts`) }  
 }
 
 export default API
