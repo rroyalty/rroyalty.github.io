4171159c9ee98997d810fab551eeb7ef78e73e85
rroyalty <rroyalty@gmail.com>
2021-07-23 18:57:03 -0400

    asdf

--- src/components/Snoo.tsx
+++ src/components/Snoo.tsx
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme: Theme) =>
             marginTop: `6px`,
             borderColor: theme.palette.light?.main,
             overflowY: `scroll`,
-            madWidth: `100%`
+            maxWidth: `100%`
         },
         img: {
             display: `flex`,
