d6a9f537cbd22100ff3be6d55e324b25bf5495eb
rroyalty <rroyalty@gmail.com>
2021-07-27 11:08:27 -0400

    refactor

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const API = {
-    getNewestText: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestText`) },
-    getNewestTitle: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestTitle`) }, 
-    getNewestDate: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestDate`) },
+    // getNewestText: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestText`) },
+    // getNewestTitle: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestTitle`) }, 
+    // getNewestDate: () => { return axios.get(`https://rrprofile.herokuapp.com/api/self/newestDate`) },
     getAllPosts: async () => { return await axios.get(`https://rrprofile.herokuapp.com/api/self/allPosts`) }  
 }
 
