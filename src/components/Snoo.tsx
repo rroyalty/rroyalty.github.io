e914d83549f3a3b0fd3f067a5f29a67916ac1e46
rroyalty <rroyalty@gmail.com>
2021-07-28 16:54:49 -0400

    date test

--- src/components/Snoo.tsx
+++ src/components/Snoo.tsx
@@ -54,7 +54,7 @@ const Snoo: React.FC = (): JSX.Element => {
             setTitle(res.data[0].title);
             setText(res.data[0].selftext);
             const date = new Date(res.data[0].created * 1000)
-            setDate(date.toUTCString());
+            setDate(`${date.getDate}, ${date.getDay}/${date.getMonth}/${date.getFullYear}`);
         }).catch((err) => {
             console.log(err)
             setLength(0);
@@ -67,7 +67,7 @@ const Snoo: React.FC = (): JSX.Element => {
             setTitle(staticResponse?.data[index].title);
             setText(staticResponse?.data[index].selftext);
             const date = new Date(staticResponse?.data[index].created * 1000)
-            setDate(date.toUTCString());
+            setDate(`${date.getDate}, ${date.getDay}/${date.getMonth}/${date.getFullYear}`);
         } else {
             setTitle(null);
             setText(null);
